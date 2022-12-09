a8026634da8ba33a6ff8121506fd1e8ae381e886
Nathan Williams <nathan.williams@codethink.co.uk>
2022-12-09 14:05:18 +0000

    day-09

---
+++ day-09/tests/tests.rs
@@ -0,0 +1,17 @@
+mod tests {
+    use day_09::day_09::solve;
+
+    #[test]
+    fn test_example_01() {
+        let x = solve("example.txt");
+        assert!(x.is_ok());
+        assert_eq!((13,1),x.unwrap());
+    }
+
+    #[test]
+    fn test_example_02() {
+        let x = solve("example2.txt");
+        assert!(x.is_ok());
+        assert_eq!((88,36),x.unwrap());
+    }
+}
